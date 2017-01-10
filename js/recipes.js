add2fa04abef731b5637ed57084385556abece6d
Hristo Hristov <archri@openmailbox.org>
2017-01-10 22:30:49 +0200

    fixed json url

--- js/recipes.js
+++ js/recipes.js
@@ -2,7 +2,7 @@
 (function() {
   var app = angular.module("recipesApp", []);
   app.controller('recipesController', function($scope, $http) {
-    $http.get("/data/recipes.json").then(function(response) {
+    $http.get("./data/recipes.json").then(function(response) {
       $scope.recipes = response.data.recipes;
     })
 
