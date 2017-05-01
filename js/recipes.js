4c470c612e63e482ee3d33730700965c655cff5f
Hristo Hristov <archri@openmailbox.org>
2017-05-01 19:42:57 +0300

    added onclick image enlarge for the recipies

--- js/recipes.js
+++ js/recipes.js
@@ -1,13 +1,6 @@
 'use strict';
-(function() {
-  var app = angular.module("recipesApp", []);
-  app.controller('recipesController', function($scope, $http) {
-    $http.get("./data/recipes.json").then(function(response) {
-      $scope.recipes = response.data.recipes;
-    })
-
-    $scope.selectRecipe = function(recipe) {
-      $scope.selected = recipe;
-    }
-  });
-})();
+console.log($(".image-small"));
+$(".image-small").click(function (event) {
+  $("#image-placeholder").attr("src", $(this).find("img").attr("src"));
+  return false;
+});
